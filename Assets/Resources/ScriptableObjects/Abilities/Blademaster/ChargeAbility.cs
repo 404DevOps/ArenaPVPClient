ca287ba7beea6127362e55e6c8ce8fffd00c1871
Simon Schmid <404devops@gmail.com>
2024-06-28 15:31:27 +0200

    feat: added charge ability

--- Assets/Resources/ScriptableObjects/Abilities/Blademaster/ChargeAbility.cs
+++ Assets/Resources/ScriptableObjects/Abilities/Blademaster/ChargeAbility.cs
@@ -1,7 +1,11 @@
+using Assets.Scripts.Enums;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+
+[CreateAssetMenu(menuName = "Abilities/Blademaster/ChargeAbility", fileName = "ChargeAbility")]
 public class ChargeAbility : CharacterAbility
 {
     public float chargeSpeed;
